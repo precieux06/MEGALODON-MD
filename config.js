5bc0db265337faeaa55ab13b9415e35cb401b188
precieux06 <jmakumbusela@gmail.com>
2025-08-22 22:42:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
 
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~wBBA0IqI#yspZWAr5lmX4OqBv3xCdEggEOGAUhK9vgw9jBDbbZzU",  // Your bot's session ID (keep it secure)
     PREFIX: process.env.PREFIX || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",  // Owner's WhatsApp number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "243982730192",  // Owner's WhatsApp number
     
-    OWNER_NAME: process.env.OWNER_NAME || "",           // Owner's name
+    OWNER_NAME: process.env.OWNER_NAME || "PRECIEUX",           // Owner's name
     
     DEV: process.env.DEV || "50948336180",                     // Developer's contact number
     
